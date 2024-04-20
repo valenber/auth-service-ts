3def9c8e79e0884507424136ad88b99d01b3c4d4
Valentin Berlin <valentin.berlin@protonmail.com>
2024-04-20 22:58:53 +0200

    update jest config to resolve import from src folder

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   clearMocks: true,
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
+  moduleDirectories: ['node_modules', 'src']
 };
